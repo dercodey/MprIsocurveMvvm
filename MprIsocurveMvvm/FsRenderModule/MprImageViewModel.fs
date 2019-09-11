fb27fefe943aea9fabe3a68b261a262505ffa65d
Lane, Derek <derek.lane@elekta.com>
2019-09-11 12:18:44 -0500

    merging fsharp mpr image viewmodel

---
+++ MprIsocurveMvvm/FsRenderModule/MprImageViewModel.fs
@@ -0,0 +1,46 @@
+﻿namespace FsRenderModule.ViewModels
+
+open System
+open System.Threading.Tasks
+open System.Windows.Media.Imaging
+open System.Windows.Media
+open Prism.Mvvm
+open Infrastructure.Interfaces
+open FsRenderModule.Interfaces
+
+type MprImageViewModel(repository:IModelRepository, mprGenerator:IMprGenerationFunction) as this =
+    inherit BindableBase()
+
+    let updateImageSource (pixels:byte[,])  =
+        let height, width = pixels.GetLength(0), pixels.GetLength(1)
+        this.MprBuffer <- 
+            let flatPixelAt n = pixels.[int (n/width), n % width]
+            let flatPixels = Array.init<byte> (height * width) flatPixelAt
+            BitmapSource.Create(width, height, 96.0, 96.0, PixelFormats.Gray8, null, flatPixels, width)
+        this.ImagePosition <- new TranslateTransform(-float width / 2.0, -float height / 2.0)
+
+    let mutable _mprBuffer : ImageSource = null
+    let mutable _imagePosition : Transform = null
+
+    member val VolumeGuid = Guid.Empty with get, set
+    member this.MprBuffer
+        with get() = _mprBuffer
+        and set (value:ImageSource) = 
+            base.SetProperty(ref _mprBuffer, value) |> ignore
+    member this.ImagePosition
+        with get() = _imagePosition
+        and set (value:Transform) = 
+            base.SetProperty(ref _imagePosition, value) |> ignore
+    //member this.SetPerformanceCounter(counter:PerformanceCounter)
+    //    _counter = counter;
+    //PerformanceCounter _counter;
+
+    interface IRenderedObject with
+        member this.UpdateRenderedObject(orientation:Orientation, nSliceNumber:int) : Task<Action> =
+            //_counter.StartEvent();
+            let uiv = repository.GetUniformImageVolume(this.VolumeGuid);
+            let pixels = mprGenerator.GenerateMprAsync(uiv, orientation, nSliceNumber).Result
+            //_counter.EndEvent();
+
+            // perform the update on the UI thread
+            Task.FromResult<Action>(fun () -> if pixels <> null then updateImageSource(pixels) else ())
--- MprIsocurveMvvm/RenderModule/ViewModels/MprImageViewModel.cs
+++ MprIsocurveMvvm/RenderModule/ViewModels/MprImageViewModel.cs
@@ -14,41 +14,12 @@ namespace RenderModule.ViewModels
     /// <summary>
     /// 
     /// </summary>
-    public class MprImageViewModel : BindableBase, IRenderedObject
+    public class MprImageViewModel : FsRenderModule.ViewModels.MprImageViewModel
     {
-        IModelRepository _repository;
-        IMprGenerationFunction _mprGenerator;
-
         public MprImageViewModel(IModelRepository repository, IMprGenerationFunction mprGenerator)
+            : base(repository, mprGenerator)
         {
-            _repository = repository;
-            _mprGenerator = mprGenerator;
         }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public Guid VolumeGuid { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public ImageSource MprBuffer
-        {
-            get { return _mprBuffer; }
-            set { SetProperty(ref _mprBuffer, value); }
-        }
-        private ImageSource _mprBuffer;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public Transform ImagePosition
-        {
-            get { return _imagePosition; }
-            set { SetProperty(ref _imagePosition, value); }
-        }
-        private Transform _imagePosition;
 
         /// <summary>
         /// 
@@ -59,59 +30,5 @@ namespace RenderModule.ViewModels
             _counter = counter;
         }
         PerformanceCounter _counter;
-
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="orientation"></param>
-        /// <param name="nSliceNumber"></param>
-        /// <param name="uiResponseAction"></param>
-        public async Task<Action> UpdateRenderedObject(Orientation orientation, int nSliceNumber)
-        {
-            _counter.StartEvent();
-
-            var uiv = _repository.GetUniformImageVolume(VolumeGuid);
-            var pixels = await _mprGenerator.GenerateMprAsync(uiv, orientation, nSliceNumber);
-            _counter.EndEvent();
-
-            // perform the update on the UI thread
-            return (() => UpdateImageSource(pixels));
-        }
-
-        // 
-        private void UpdateImageSource(byte[,] pixels)
-        {
-            if (pixels != null)
-            {
-                int height = pixels.GetLength(0);
-                int width = pixels.GetLength(1);
-
-                byte[] flatPixels = FlattenPixels(pixels);
-                MprBuffer = BitmapSource.Create(width, height,
-                    96.0, 96.0, PixelFormats.Gray8, null, flatPixels, width);
-
-                // update the position
-                ImagePosition = new TranslateTransform(-width / 2, -height / 2);
-            }
-            else
-            {
-                MprBuffer = null;
-            }
-        }
-
-        private static byte[] FlattenPixels(byte[,] pixels)
-        {
-            int height = pixels.GetLength(0);
-            int width = pixels.GetLength(1);
-
-            // flatten the pixels
-            byte[] flatPixels = new byte[height * width];
-            int n = 0;
-            for (int r = 0; r < pixels.GetLength(0); r++)
-                for (int c = 0; c < pixels.GetLength(1); c++, n++)
-                    flatPixels[n] = pixels[r, c];
-            return flatPixels;
-        }
     }
 }
